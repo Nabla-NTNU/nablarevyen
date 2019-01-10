0e9eb5eb9bd556e8900a7dc55cca53f13a389342
Thorvald Ballestad <thorvald.tb@gmail.com>
2019-01-10 11:25:26 +0100

    First iteration towards production settings

---
+++ nablarevyen/settings/devel.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from .base import *
+import os
+
+DEBUG = True
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(VARIABLE_CONTENT, os.environ.get('NABLAREVYEN_DB', 'sqlite.db')),
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+}
+
+# All epost blir sendt til terminalen, istedet for ut til brukerne.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# easy_thumbnail debugging
+# Gjør at man får en feilmelding dersom thumbnail-taggen ikke klarer å lage ny
+# thumbnail
+THUMBNAIL_DEBUG = True

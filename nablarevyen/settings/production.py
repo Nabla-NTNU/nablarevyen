d3c3a61a240a9aa496d55c10bcd468d7c5cc9ec6
Thorvald Ballestad <thorvald.tb@gmail.com>
2019-01-10 11:25:26 +0100

    First iteration towards production settings

---
+++ nablarevyen/settings/production.py
@@ -0,0 +1,26 @@
+"""
+Production settings for nablarevyenv2
+"""
+from os import environ as env
+import pymysql
+from .base import *
+
+pymysql.install_as_MySQLdb()
+
+DEBUG = bool(env.get('DEBUG', False))
+ALLOWED_HOSTS = ['revy.nabla.no']
+
+ADMINS = (
+    ('WebSjef', 'websjef@nabla.ntnu.no')
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env.get('MYSQL_DATABASE'),
+        'USER': env.get('MYSQL_USER'),
+        'PASSWORD': env.get('MYSQL_USER_PASSWORD'),
+    }
+}

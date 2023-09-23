1e5b34bf2d4e73dbd40c520a4c436c14c20ff787
tncrdk <thnchrdjk@gmail.com>
2023-09-23 15:07:24 +0200

    fix: sort new in ascending order

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -3,7 +3,7 @@ from . models import Article
 
 class ArticleList(ListView):
     model = Article
-    queryset = Article.objects.filter(published=True).order_by('created')
+    queryset = Article.objects.filter(published=True).order_by('-created')
     paginate_by = 12
     page_kwarg = 'side'
 

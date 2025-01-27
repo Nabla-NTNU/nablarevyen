ada522794d9390c6115f61de5dbf3fc52f74c262
Kajrakso <ofjakob@gmail.com>
2025-01-27 14:18:54 +0100

    fixed some versions

--- nablarevyen/settings/production.py
+++ nablarevyen/settings/production.py
@@ -10,9 +10,7 @@ pymysql.install_as_MySQLdb()
 DEBUG = bool(env.get('DEBUG', False))
 ALLOWED_HOSTS = ['revy.nabla.no']
 
-ADMINS = (
-    ('WebSjef', 'websjef@nabla.ntnu.no')
-)
+ADMINS = [('WebSjef', 'websjef@nabla.ntnu.no')]
 
 MANAGERS = ADMINS
 
